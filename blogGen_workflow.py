d25b80c71c209b4b851df7f34c4425ed02053b35
sham <aravattusham@gmail.com>
2026-09-17 14:37:45 +0530

    added the observibility & structure outline and blog response

--- blogGen_workflow.py
+++ blogGen_workflow.py
@@ -1,29 +1,38 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser , PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+from pydantic import BaseModel , Field
 
 load_dotenv()
 model = ChatGroq(model="openai/gpt-oss-120b")
 parser = StrOutputParser()
 
+class BlogResponse(BaseModel):
+    blog: str = Field(description='this field of blog')
+
+class OutlineResponse(BaseModel):
+    outline: str = Field(description='this field of outline')
+
 
 def llm_outline_gen(state:PromptChainingState):
     topic = state['topic']
-    prompt = PromptTemplate(template='Generate the outline about the {topic} ',input_variables={'topic'})
-    chain = prompt | model | parser
-    state['outline'] = chain.invoke({'topic': topic})
+    prompt = PromptTemplate(template='Generate the outline about the {topic},{format_instructions}',input_variables=['topic','format_instructions'])
+    outline_parser = PydanticOutputParser(pydantic_object=OutlineResponse)
+    chain = prompt | model | outline_parser
+    state['outline'] = chain.invoke({'topic': topic,'format_instructions':outline_parser.get_format_instructions()}).outline
     return state
 
 def llm_blog_gen(state:PromptChainingState):
     topic = state['topic']
     outline = state['outline']
-    prompt = PromptTemplate(template='Generate the blog about the {topic} and hear is the given outline \n {outline} ',
-                            input_variables=['topic','outline'])
-    chain = prompt | model | parser
-    state['blog'] = chain.invoke({'topic': topic,'outline':outline})
+    prompt = PromptTemplate(template='Generate the blog about the {topic} and hear is the given outline \n {outline} \n {format_instructions} ',
+                            input_variables=['topic','outline','format_instructions'])
+    blog_parser = PydanticOutputParser(pydantic_object=BlogResponse)
+    chain = prompt | model | blog_parser
+    state['blog'] = chain.invoke({'topic': topic,'outline':outline,'format_instructions':blog_parser.get_format_instructions()}).blog
     return state
 
 #%%
